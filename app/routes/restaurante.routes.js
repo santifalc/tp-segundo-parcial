54b62e69fffd885c6467c0ea0fff4e51dd65ae3d
Santiago Falcon <santiago.falcon141@gmail.com>
2023-05-13 18:27:42 -0400

    Se agrega put y delete de restaurante

--- app/routes/restaurante.routes.js
+++ app/routes/restaurante.routes.js
@@ -3,7 +3,8 @@ module.exports = app => {
     const rest = require("../controllers/restauranteDAO.controller.js");
     var router = require("express").Router();
     router.post("/", rest.create);
-    // router.get("/", rest.findAll);
-    // router.get("/:id", rest.findOne);
+    router.get("/", rest.findAll);
+    router.put("/:id", rest.update);
+    router.delete("/:id", rest.eliminar);
     app.use('/api/restaurante', router);
 };
