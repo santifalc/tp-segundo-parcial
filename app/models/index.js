a531b9e03e11cd665e808202dbb2068327d5f4d4
Claudia Nunez <claudia.nunez@university.mentormate.com>
2023-05-16 18:03:23 -0400

    Se agrega claves foranea a la tabla para impresion en el modelo 'mesas' por restaurante

--- app/models/index.js
+++ app/models/index.js
@@ -1,16 +1,16 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
-    port: dbConfig.PORT,
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
-    }
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+  port: dbConfig.PORT,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
 });
 
 const db = {};
@@ -18,22 +18,25 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.Ventas = require("./venta.model.js")(sequelize, Sequelize);
 //Se agregan los modelos (Crear las tablas en la bd)
-db.cliente = require('./cliente.model')(sequelize, Sequelize);
-db.restaurante = require('./restaurante.model')(sequelize, Sequelize);
-db.mesa = require('./mesa.model')(sequelize, Sequelize);
-db.posicion = require('./posicion.model')(sequelize, Sequelize);
-db.reserva = require('./reserva.model')(sequelize, Sequelize);
+db.cliente = require("./cliente.model")(sequelize, Sequelize);
+db.restaurante = require("./restaurante.model")(sequelize, Sequelize);
+db.mesa = require("./mesa.model")(sequelize, Sequelize);
+db.posicion = require("./posicion.model")(sequelize, Sequelize);
+db.reserva = require("./reserva.model")(sequelize, Sequelize);
 
 //Se agregan las relaciones necesarias para la base de datos
-db.restaurante.hasMany(db.mesa, { as: 'mesas' });
+db.restaurante.hasMany(db.mesa, { as: "mesas" });
 
-db.cliente.hasOne(db.reserva, { as: 'cliente' });
-db.restaurante.hasOne(db.reserva, { as: 'restaurante' });
-db.mesa.hasMany(db.reserva, { as: 'mesas' });
+db.cliente.hasOne(db.reserva, { as: "cliente" });
+db.restaurante.hasOne(db.reserva, { as: "restaurante" });
+db.mesa.hasMany(db.reserva, { as: "mesas" });
 // db.mesa.hasOne(db.posicion, { as: 'posicion' });
 
 //Se agregan las claves foráneas a las tablas (no hace falta xd)
-// db.mesa.belongsTo(db.restaurante, { foreignKey: 'idRestaurante', as: 'idRestaurante' });
+db.mesa.belongsTo(db.restaurante, {
+  foreignKey: "restauranteId",
+  as: "restauranteAssociation",
+});
 // db.posicion.belongsTo(db.mesa, { foreignKey: 'idPosicion', as: 'idPosicion' });
 
 // db.reserva.belongsTo(db.restaurante, { foreignKey: 'restauranteId', as: 'idRestaurante' });
