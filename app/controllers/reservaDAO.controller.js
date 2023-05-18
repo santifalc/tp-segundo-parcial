093ac1e8ded9d0bb98e7fa956c37314916a9e4c6
Santiago Falcon <santiago.falcon141@gmail.com>
2023-05-17 20:09:33 -0400

    Se agrega filtro de reserva por restaurante y por fecha

--- app/controllers/reservaDAO.controller.js
+++ app/controllers/reservaDAO.controller.js
@@ -3,7 +3,7 @@ const reservas = db.reserva;
 const Op = db.Sequelize.Op;
 exports.create = (req, res) => {
 // Validate request
-    if (!req.body.clienteId) {
+    if (!req.body.clienteCedula) {
         res.status(400).send({
             message: "Debe enviar el nombre del restaurante!"
         });
@@ -13,7 +13,7 @@ exports.create = (req, res) => {
     const request = {
         restauranteId: req.body.restauranteId,
         mesaId: req.body.mesaId,
-        clienteId: req.body.clienteId,
+        clienteCedula: req.body.clienteCedula,
         fecha: req.body.fecha,
         rangoHora: req.body.rangoHora,
         cantidadSolicitada: req.body.cantidadSolicitada,
@@ -27,6 +27,87 @@ exports.create = (req, res) => {
             res.status(500).send({
                 message:
                     err.message || "Ha ocurrido un error al crear una venta."
+            });
+        });
+};
+
+exports.findAll = (req, res) => {
+    reservas.findAll()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Ocurrio un error al obtener los reservas."
+            });
+        });
+};
+
+exports.findAllByRestaurante = (req, res) => {
+    if (!req.body.restauranteId) {
+        res.status(400).send({
+            message: "Debe seleccionar un restaurante!",
+        });
+        return;
+    }
+
+    const restauranteId = req.body.restauranteId;
+
+    reservas
+        .findAll({where: {restauranteId},
+            order: [['clienteCedula', 'ASC'], ['mesaId', 'ASC']]
+        })
+        .then((data) => {
+            if (data.length > 0) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message:
+                        "No se han encontrado reservas para el restaurante con ID " +
+                        restauranteId,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message:
+                    "Error al obtener mesas del restaurante con ID " + restauranteId,
+                error: err.message,
+            });
+        });
+};
+
+exports.findAllByFecha = (req, res) => {
+    if (!req.body.fecha) {
+        res.status(400).send({
+            message: "Debe seleccionar una fecha!",
+        });
+        return;
+    }
+
+    const fecha = req.body.fecha;
+
+    reservas
+        .findAll({where: {fecha},
+            order: [['clienteCedula', 'ASC'], ['mesaId', 'ASC']]
+        })
+        .then((data) => {
+            if (data.length > 0) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message:
+                        "No se han encontrado reservas con fecha " +
+                        fecha,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message:
+                    "Error al obtener reservas con fecha " + fecha,
+                error: err.message,
             });
         });
 };
--- app/routes/reserva.routes.js
+++ app/routes/reserva.routes.js
@@ -3,7 +3,9 @@ module.exports = app => {
     const reserva = require("../controllers/reservaDAO.controller.js");
     var router = require("express").Router();
     router.post("/", reserva.create);
-    // router.get("/", mesa.findAll);
+    router.get("/", reserva.findAll);
+    router.post("/filtro-restaurante", reserva.findAllByRestaurante);
+    router.post("/filtro-fecha", reserva.findAllByFecha);
     // router.get("/:id", mesa.findOne);
     app.use('/api/reserva', router);
 };
