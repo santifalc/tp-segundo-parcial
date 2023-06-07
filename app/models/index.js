853c19a6606f0cde1c6a9816bd222ae5226f6af1
Claudia Nunez <claudia.nunez@university.mentormate.com>
2023-06-07 07:46:18 -0400

    Se cambia relacion consumoCabecera x 1 a muchos con dellateConsumo, se agrega consumoCabecera routes

--- app/models/index.js
+++ app/models/index.js
@@ -36,7 +36,7 @@ db.restaurante.hasOne(db.reserva, { as: "restaurante" });
 db.mesa.hasMany(db.reserva, { as: "mesas" });
 db.posicion.hasOne(db.mesa, { as: "posicion" });
 db.categorias.hasOne(db.productos, { as: "categorias" });
-db.consumoCabecera.hasOne(db.detalleConsumo, { as: "consumo_cabecera" });
+db.consumoCabecera.hasMany(db.detalleConsumo, { as: "consumo_cabecera" });
 
 //Se agregan las claves foráneas a las tablas (no hace falta xd)
 // db.mesa.belongsTo(db.restaurante, {
